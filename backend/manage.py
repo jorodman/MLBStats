7bebb635747e0a0cb2b804750262fcb335b69164
Joseph Rodman <josephrodman5@gmail.com>
2022-07-19 21:40:07 -0400

    got rid of exception

--- backend/manage.py
+++ backend/manage.py
@@ -16,7 +16,7 @@ def main():
 
     try:
         from django.core.management import execute_from_command_line
-        raise Exception("Test")
+        # raise Exception("Test")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
