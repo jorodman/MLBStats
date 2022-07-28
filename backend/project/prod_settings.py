f14260d172b557ff24aa600e91e0128cf0918ba1
Joseph Rodman <josephrodman5@gmail.com>
2022-07-27 20:58:50 -0400

    added prod settings

--- backend/project/prod_settings.py
+++ backend/project/prod_settings.py
@@ -3,11 +3,11 @@ from .base_settings import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    # '127.0.0.1',
-    # 'localhost',
-    # '172.31.31.228',
-    # '18.218.230.112',
-    '3.22.56.148'
+    '127.0.0.1',
+    'localhost',
+    '172.31.31.228',
+    '18.218.230.112',
+    '3.22.56.148',
     ]
 
 CORS_ALLOWED_ORIGINS = ['http://mlbstats.s3-website.us-east-2.amazonaws.com']
